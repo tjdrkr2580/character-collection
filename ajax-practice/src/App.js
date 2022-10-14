6d556d398ea408a41b72b3d2e44f39dfee28860b
Kim Tae Hyun <tjdrkr2580@naver.com>
2022-10-14 14:51:24 +0900

    추가 jsons, setting

--- ajax-practice/src/App.js
+++ ajax-practice/src/App.js
@@ -1,5 +1,20 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Web</h1>
+      <nav>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JS</li>
+        </ul>
+      </nav>
+      <article>
+        <h2>Welcome</h2>
+        Hello, React &amp; Ajax
+      </article>
+    </div>
+  );
 }
 
 export default App;
